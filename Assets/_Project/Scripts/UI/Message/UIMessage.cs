fe5ba8c30e0869113cce04e188b6f275ecbdfe60
Bartłomiej Puchniewicz <bartlomiej.puchniewicz@gmail.com>
2025-06-17 03:11:54 +0200

    feat(UI): add fade-out animation for messages

--- Assets/_Project/Scripts/UI/Message/UIMessage.cs
+++ Assets/_Project/Scripts/UI/Message/UIMessage.cs
@@ -7,12 +7,14 @@ namespace AE
     public class UIMessage : MonoBehaviour
     {
         private VisualElement _root;
-        private Label _message;
+        private Label _messageLabel;
 
         private void Awake()
         {
             _root = GetComponent<UIDocument>().rootVisualElement;
-            _message = _root.Q<Label>("message-label");
+
+            _messageLabel = _root.Q<Label>("message-label");
+            _messageLabel.AddToClassList("transition-message");
 
             HideMessage();
         }
@@ -26,8 +28,18 @@ namespace AE
                 return;
             }
 
-            _message.text = text;
+            if (_messageLabel.ClassListContains("fade-out"))
+            {
+                _messageLabel.RemoveFromClassList("fade-out");
+            }
+
+            _messageLabel.text = text;
             _root.style.display = DisplayStyle.Flex;
+
+            _messageLabel.schedule.Execute(() =>
+            {
+                _messageLabel.AddToClassList("fade-out");
+            }).ExecuteLater(4000); // 4 seconds delay
 
             callback?.Invoke();
         }
